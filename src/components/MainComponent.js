ca427c3b24a7377382aad83aca8bb514ada1470d
Neel Thakker <neelthakker2001@gmail.com>
2020-08-25 16:09:07 +0530

    Functional Components

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Menu from './Menu_functionalComponent';
 import DishDetail from './Dishdetail_functionalComponent';
 import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
 
 class Main extends Component {
 
@@ -21,11 +22,31 @@ class Main extends Component {
   }
 
   render() {
+
+    const HomePage = () => {
+      return(
+          <Home 
+          />
+      );
+    }
+
+    const MenuPage = () => {
+      return (
+        <div>
+        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
+        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        </div>
+      );
+    }
+
     return (
       <div>
         <Header />
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+          <Switch>
+              <Route path='/home' component={HomePage} />
+              <Route exact path='/menu' component={MenuPage} />
+              <Redirect to="/home" />
+          </Switch>
         <Footer />
       </div>
     );
